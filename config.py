6b6a2256000046fc4f377c77cf2daeee9b7edbcd
LEDev <support@leltd.ru>
2023-08-12 16:37:27 +0300

    Added languages

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 # API Key from https://serpapi.com
 api_key = 'API_KEY'
+# Lang (EN/RU)
+lang = 'EN'
--- main.py
+++ main.py
@@ -8,6 +8,94 @@ import config
 
 
 def main():
+    if config.lang == 'EN':
+        main_en()
+    elif config.lang == 'RU':
+        main_ru()
+    else:
+        print('Selected incorrect lang (in config.py)')
+
+
+def main_en():
+    req = input("Write request: ")
+    url = 'https://serpapi.com/search.json'
+
+    params = {
+        "engine": "youtube",
+        "search_query": req,
+        "gl": "us",
+        "hl": "en",
+        "api_key": config.api_key
+    }
+
+    r = requests.get(url, params=params)
+
+    r_json = r.json()
+
+    folder_name = 'history'
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+    date_info = datetime.datetime.now()
+    file_name = 'request_{0}.{1}.{2}_{3}.{4}.{5}.{6}.json'.format(str(date_info.year),
+                                                                  str(date_info.month),
+                                                                  str(date_info.day),
+                                                                  str(date_info.hour),
+                                                                  str(date_info.minute),
+                                                                  str(date_info.second),
+                                                                  str(date_info.microsecond))
+
+    with open(folder_name + os.path.sep + file_name, 'w', encoding='utf-8') as f:
+        json.dump(r_json, f, ensure_ascii=False, indent=4)
+
+    try:
+        playlist_results = r_json['playlist_results']
+        # print(playlist_results)
+    except KeyError:
+        playlist_results = None
+
+    try:
+        video_results = r_json['video_results']
+        # print(video_results)
+    except KeyError:
+        video_results = None
+
+    print('  Content of request "' + req + '":')
+
+    if playlist_results is not None:
+        print('Playlists:')
+        for i in range(10):
+            try:
+                playlist = playlist_results[i]
+                print(str(i + 1) + ". " + playlist['title'])
+                print('Author: ' + playlist['channel']['name'])
+                print('Link to channel: ' + playlist['channel']['link'])
+                print('Video count: ' + str(playlist['video_count']))
+                print('Link: https://www.youtube.com/playlist?' + str(playlist['link']).split('&')[1])
+                print('Watch: ' + playlist['link'])
+            except:
+                pass
+
+    if video_results is not None:
+        if playlist_results is not None:
+            print('')
+        print('Videos:')
+        for i in range(10):
+            try:
+                video = video_results[i]
+                print(str(i + 1) + ". " + video['title'])
+                print('Author: ' + video['channel']['name'])
+                print('Link to channel: ' + video['channel']['link'])
+                print('Posted ' + video['published_date'])
+                print('Views: ' + str(video['views']))
+                print('Length: ' + video['length'])
+                print('Description: ' + video['description'])
+                print('Watch: ' + video['link'])
+            except:
+                pass
+
+
+def main_ru():
     req = input("Введите запрос: ")
     url = 'https://serpapi.com/search.json'
 
